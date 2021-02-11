b6c137ed53db0be9f41488c645e55154e9c390ae
DJWOMS <socanime@gmail.com>
2021-02-11 21:23:37 +0300

    first commit

---
+++ Player2.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+
+export var speed = 400 
+var screen_size
+
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "jump"
+		$AnimatedSprite.flip_v = velocity.y > 0
---
+++ Spatial.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+signal hit
+
+export var speed = 400 
+var velocity = Vector2(0, 0)
+const SPEED = 100
+const GRAVITY = 35
+const JUMPFORCE = -1000
+var screen_size
+
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
+
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		
+	velocity.y = velocity.y + GRAVITY
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMPFORCE
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(velocity.x ,0, 0.2)
+	
+	""" if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "jump"
+		$AnimatedSprite.flip_v = velocity.y > 0 """
