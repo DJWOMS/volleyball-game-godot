6654c9e19ab1d7bdc49555bc24cbd732ef8f881e
DJWOMS <socanime@gmail.com>
2021-02-12 16:24:34 +0300

    add score & players turn

--- Ball.gd
+++ Ball.gd
@@ -1,17 +1,28 @@
 extends RigidBody2D
+signal fall_pl_1
+signal fall_pl_2
+
 
 func _ready():
-	pass
+	if GlobalScore.player_turn == 2:
+		position.x = 262
+	elif GlobalScore.player_turn == 1:
+		position.x = 783
 	
 	
 func _on_fallzone_pl1_body_entered(body):
 	if body is RigidBody2D:
+		emit_signal("fall_pl_1")
+		GlobalScore.player_turn = 2
 		get_tree().change_scene("res://main.tscn")
 		
 		
 func _on_fallzone_pl2_body_entered(body):
 	if body is RigidBody2D:
+		emit_signal("fall_pl_2")
+		GlobalScore.player_turn = 1
 		get_tree().change_scene("res://main.tscn")
+		
 
 
 
---
+++ GlobalScore.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+var score_pl_1 = 0
+var score_pl_2 = 0
+var player_turn = 2
